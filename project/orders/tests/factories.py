511b18f96ccecf1e718847439b1a0f987f314a0d
Joseph Victor Zammit <jvzammit@gmail.com>
2023-09-11 18:00:26 +0200

    approach #3 override Factory._create

--- project/orders/tests/factories.py
+++ project/orders/tests/factories.py
@@ -4,3 +4,12 @@ import factory
 class OrderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "orders.Order"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        # override auto_now_add for "created_at" column
+        created_at = kwargs.pop("created_at", None)
+        obj = super()._create(model_class, *args, **kwargs)
+        if created_at is not None:
+            model_class.objects.filter(id=obj.id).update(created_at=created_at)
+        return obj
--- project/orders/tests/test_models.py
+++ project/orders/tests/test_models.py
@@ -1,29 +1,8 @@
 import arrow
-import contextlib
 import time_machine
 from django.test import TestCase
 from orders.models import Order
 from orders.tests.factories import OrderFactory
-
-
-@contextlib.contextmanager
-def suppress_autotime(model, fields):
-    _original_values = {}
-    for field in model._meta.local_fields:
-        if field.name in fields:
-            _original_values[field.name] = {
-                'auto_now': field.auto_now,
-                'auto_now_add': field.auto_now_add,
-            }
-            field.auto_now = False
-            field.auto_now_add = False
-    try:
-        yield
-    finally:
-        for field in model._meta.local_fields:
-            if field.name in fields:
-                field.auto_now = _original_values[field.name]['auto_now']
-                field.auto_now_add = _original_values[field.name]['auto_now_add']
 
 
 class OrderQuerysetTest(TestCase):
@@ -34,11 +13,10 @@ class OrderQuerysetTest(TestCase):
         date2 = arrow.get("2023-09-10 00:00").datetime  # today 00:00
         date3 = arrow.get("2023-09-10 00:01").datetime  # today 00:01
         
-        # approach 2
-        with suppress_autotime(Order, "created_at"):
-            _ = OrderFactory(created_at=date1)
-            order2 = OrderFactory(created_at=date2)
-            order3 = OrderFactory(created_at=date3)
+        # approach 3
+        _ = OrderFactory(created_at=date1)
+        order2 = OrderFactory(created_at=date2)
+        order3 = OrderFactory(created_at=date3)
 
         # When created_today is called
         queryset = Order.objects.all().created_today()
