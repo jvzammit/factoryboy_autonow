a957bc67023c5c47788dcfee9edb90d10050a18b
Joseph Victor Zammit <jvzammit@gmail.com>
2023-09-11 17:31:22 +0200

    approach #1 using model.save()

--- project/orders/tests/test_models.py
+++ project/orders/tests/test_models.py
@@ -12,9 +12,15 @@ class OrderQuerysetTest(TestCase):
         date1 = arrow.get("2023-09-09 23:59").datetime  # yesterday
         date2 = arrow.get("2023-09-10 00:00").datetime  # today 00:00
         date3 = arrow.get("2023-09-10 00:01").datetime  # today 00:01
-        _ = OrderFactory(created_at=date1)
-        order2 = OrderFactory(created_at=date2)
-        order3 = OrderFactory(created_at=date3)
+        
+        # approach 1
+        order1, order2, order3 = OrderFactory.create_batch(3)
+        order1.created_at = date1
+        order1.save()
+        order2.created_at = date2
+        order2.save()
+        order3.created_at = date3
+        order3.save()
 
         # When created_today is called
         queryset = Order.objects.all().created_today()
